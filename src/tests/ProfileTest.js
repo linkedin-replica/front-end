5388c9ac2e3be4538f2f0afd4cda372375e4ba47
Rana Saeed <ranas.2122@gmail.com>
2018-04-19 23:24:30 +0200

    modified profile main view

--- src/tests/ProfileTest.js
+++ src/tests/ProfileTest.js
@@ -30,6 +30,7 @@ class ProfileTest extends Component {
                             id="1" />
               </section>
             </div>
+            
             <div style = {styles.left}>
               <section>
                 <ProfileExperience sectionTitle="Experience" data={mockExperienceSection} />
@@ -52,9 +53,6 @@ class ProfileTest extends Component {
 }
 
 const styles = {
-  header: {
-      
-  },
   header: {
     width: '100%',
     float: 'left',
